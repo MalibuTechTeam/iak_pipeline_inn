604747dc68434655027ff2dd919ab297723697c6
Moldrok <alessio.aquilano@outlook.it>
2023-08-28 17:51:54 +0200

    feat: add initial release

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+lua54        'yes'
+game         'gta5'
+
+name 'Pipeline Inn'
+author 'Malibù Tech'
+version      '1.0.0'
+description 'A meticulously crafted surf-themed restaurant, perfectly nestled by the beach. Immerse yourself in the coastal ambiance, with stunning ocean views and an inviting atmosphere that captures the essence of sun, sand, and surf.'
+
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/iak_pipeline_inn_game.dat'
+
+files {
+  "interiorproxies.meta",
+  'audio/iak_pipeline_inn_game.dat151.rel',
+}
+
+--COORDS: -2207.11, -388.71, 14.11
+
