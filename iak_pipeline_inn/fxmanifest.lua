f3966f6255ed4d34cba2ebe935ec4104a307af1c
Iakko <63969450+AndreIakko@users.noreply.github.com>
2023-08-28 17:37:05 +0200

    Add files via upload

---
+++ iak_pipeline_inn/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version "cerulean"
+
+game "gta5"
+
+author 'Iakko'
+
+description "Pipeline Inn"
+
+discord "https://discord.gg/tqk3kAEr4f"
+
+this_is_a_map 'yes'
+
+--cord: -2207.11, -388.71, 14.11
+
+data_file 'AUDIO_GAMEDATA' 'audio/iak_pipeline_inn_game.dat'
+
+files {
+    "interiorproxies.meta",
+    'audio/iak_pipeline_inn_game.dat151.rel',
+  }
+
+
+
+
