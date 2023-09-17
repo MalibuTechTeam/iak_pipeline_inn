cc6a2a8a943bf3392d10bcef5c2a494aa5b39974
Moldrok <alessio.aquilano@outlook.it>
2023-09-17 17:50:18 +0200

    feat(core): add version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,8 +8,11 @@ version      '1.0.0'
 description 'A meticulously crafted surf-themed restaurant, perfectly nestled by the beach. Immerse yourself in the coastal ambiance, with stunning ocean views and an inviting atmosphere that captures the essence of sun, sand, and surf.'
 
 this_is_a_map 'yes'
-
 data_file 'AUDIO_GAMEDATA' 'audio/iak_pipeline_inn_game.dat'
+
+server_scripts {
+  'core/server.lua'
+}
 
 files {
   "interiorproxies.meta",
